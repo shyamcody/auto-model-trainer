bf9ef7f52f3e00e7b8cdee15b0c68bad0e164950
shyambhu mukherjee <46677829+shyamcody@users.noreply.github.com>
2020-12-16 18:43:19 +0530

    Update predictor_app.py

--- predictor_app.py
+++ predictor_app.py
@@ -21,29 +21,29 @@ if uploaded_file is not None:
     #to read file as bytes:
     dataframe = pd.read_csv(uploaded_file)
     st.write("thanks for uploading the file. Processing...")
+
+    st.subheader("the data looks like this:")
+    st.write(dataframe.head())
+    st.subheader("Name the target column")
+    target_column = st.text_input("Target column name","Example: Target")
+    Y = dataframe[target_column]
+    X = dataframe.drop(target_column,axis = 1)
+    st.write("choose the train_test_split value")
+    ratio = st.slider("How much test percentage to keep:",0.01,0.5,0.01)
+    st.write("ratio chosen is:",ratio)
+    X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = ratio, random_state = 42)
+
+    if action == 'classification':
+        from lazypredict.Supervised import LazyClassifier
+        clf = LazyClassifier(verbose = 0, ignore_warnings = True,custom_metric = None)
+        models,predictions = clf.fit(X_train,X_test,Y_train,Y_test)
+        st.write(models)
+    elif action == 'regression':
+        from lazypredict.Supervised import LazyRegressor
+        reg = LazyRegressor(verbose = 0, ifnore_warnings = False, custom_metric = None)
+        models,predictions = reg.fit(X_train,X_test,Y_train,Y_test)
+        st.write(models)
+    st.write("Now, you can choose the best model and go on modeling on your own")
+    st.write("thanks for visiting")
 else:
     st.write("You do have to upload a file to proceed")
-st.subheader("the data looks like this:")
-st.write(dataframe.head())
-st.subheader("Name the target column")
-target_column = st.text_input("Target column name","Example: Target")
-Y = dataframe[target_column]
-X = dataframe.drop(target_column,axis = 1)
-st.write("choose the train_test_split value")
-ratio = st.slider("How much test percentage to keep:",0.01,0.5,0.01)
-st.write("ratio chosen is:",ratio)
-X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = ratio, random_state = 42)
-
-if action == 'classification':
-    from lazypredict.Supervised import LazyClassifier
-    clf = LazyClassifier(verbose = 0, ignore_warnings = True,custom_metric = None)
-    models,predictions = clf.fit(X_train,X_test,Y_train,Y_test)
-    st.write(models)
-elif action == 'regression':
-    from lazypredict.Supervised import LazyRegressor
-    reg = LazyRegressor(verbose = 0, ifnore_warnings = False, custom_metric = None)
-    models,predictions = reg.fit(X_train,X_test,Y_train,Y_test)
-    st.write(models)
-st.write("Now, you can choose the best model and go on modeling on your own")
-st.write("thanks for visiting")
-
